ad73ac1b59129f7fc57f6f27f566f2e84e669997
Otaku <151172326+Elian-Ch-02@users.noreply.github.com>
2025-08-09 22:12:45 +0200

    Lab 5
    Control nominas

---
+++ Lab#5Control de Nominas/src/lab/pkg5control/de/nominas/Empleado.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg5control.de.nominas;
+
+/**
+ *
+ * @author UCC
+ */
+public class Empleado {
+    private String nombre;
+    private String apellido;
+    private double salarioDia;
+    private double otrosIngresos;
+    private double pagoCCSS;
+    private double aportePensiones;
+    private int diasTrabajados;
+    private TipoCargo cargo;
+    private TipoGenero genero;
+    
+    public Empleado(String nomnre, String apellido, double salarioDia, double otrosIngresos, double pagoCCSS, double aportePensiones, int diasTrabajados, TipoCargo cargo, TipoGenero Genero){
+        this.nombre = nomnre;
+        this.apellido = apellido;
+        this.salarioDia = salarioDia;
+        this.otrosIngresos = otrosIngresos;
+        this.pagoCCSS = pagoCCSS;
+        this.aportePensiones = aportePensiones;
+        this.diasTrabajados = diasTrabajados;
+        this.cargo = cargo;
+        this.genero = genero; 
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellidos(){
+        return apellido;
+    }
+    public double getSalarioDia(){
+        return salarioDia;
+    }
+    public double getOtrosIngresos(){
+        return otrosIngresos;
+    }
+    public double getPagoCCSS(){
+        return pagoCCSS;
+    }
+    public double getApotePenciones(){
+        return aportePensiones;
+    }
+    public int getDiasTrabajos(){
+        return diasTrabajados;
+    }
+    public TipoCargo getCargo(){
+        return cargo;
+    }
+    public TipoGenero getGenero(){
+        return genero;
+    }
+    
+    public double calcularNomina(){
+        return ((salarioDia * diasTrabajados) + otrosIngresos - pagoCCSS - aportePensiones);
+    }
+    
+}
---
+++ Lab#5Control de Nominas/src/lab/pkg5control/de/nominas/ListaEmpleados.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg5control.de.nominas;
+
+/**
+ *
+ * @author UCC
+ */
+public class ListaEmpleados {
+    
+    public Vector lista;
+    
+    public double totalNomina = 0;
+    
+    public void listaEmpledo(Empleado a){
+        lista.add(a);
+    }
+    
+}
